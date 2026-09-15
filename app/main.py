dfaaa3be8b2cd3d5007ab8986e8ceda83af751f7
Ruslan <sounditbox@gmail.com>
2026-09-15 20:30:10 +0400

    base app created

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
+from contextlib import asynccontextmanager
 
+from fastapi import FastAPI
+
+
+@asynccontextmanager
+def lifespan(app: FastAPI):
+    yield
+
+
+def create_app() -> FastAPI:
+    app = FastAPI(lifespan=lifespan)
+    return app
+
+
+app = create_app()
