d7a428417e6a4bcd4e4733768b0d796d22bfc0fa
Ruslan <sounditbox@gmail.com>
2026-09-22 21:24:05 +0400

    feat: add habrparser

---
+++ app/parsers/habr_parser.py
@@ -0,0 +1,49 @@
+from datetime import datetime
+from pprint import pprint
+import asyncio
+import httpx
+
+from app.parsers.base_parser import SiteParser
+import xml.etree.ElementTree as ET
+
+
+class HabrParser(SiteParser):
+    parse_url = 'https://habr.com/ru/rss/articles/'
+
+    def can_handle(self, url: str) -> bool:
+        return url == self.parse_url
+
+    async def parse(self, url: str, limit: int=10) -> list[dict]:
+        async with httpx.AsyncClient() as client:
+            response = await client.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+
+        articles = []
+        root = ET.fromstring(response.text)
+        for item in root.findall("./channel/item")[:limit]:
+            title = item.find('title').text
+            link = item.find('link').text
+            raw_description = item.find('description').text
+            description = self.clean_html(raw_description)
+
+            if not description or not link:
+                continue
+
+            articles.append(
+                {
+                    'title': title,
+                    'link': link,
+                    'description': description,
+                    'collected_at': datetime.now(),
+                }
+            )
+        print(len(articles))
+        return articles
+
+    def clean_html(self, raw_description):
+        # TODO: implement html cleaning
+        return raw_description
+
+
+if __name__ == '__main__':
+    parser = HabrParser()
+    asyncio.run(parser.parse(parser.parse_url))
